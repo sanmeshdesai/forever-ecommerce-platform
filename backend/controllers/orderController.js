2b10f0d5e054da4d5e0d816652a2c9b7f372b328
sanmesh <sanmeshdesai11@gmail.com>
2026-07-01 14:45:22 +0530

    Configure stripe webhook

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -171,4 +171,4 @@ const updateStatus = async (req, res) => {
     
 }
 
-export { placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus, verifyStripe}
+export { placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus}
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus, verifyStripe} from '../controllers/orderController.js'
+import { placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus} from '../controllers/orderController.js'
 import adminAuth from '../middleware/adminAuth.js'
 import authUser from '../middleware/auth.js'
 
@@ -18,6 +18,5 @@ orderRouter.post('/razorpay', authUser, placeOrderRazorpay)
 orderRouter.post('/userorders', authUser, userOrders)
 
 //verify
-orderRouter.post('/verifyStripe',authUser, verifyStripe)
 
 export default orderRouter
