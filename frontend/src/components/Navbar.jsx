530ff35917bcaed8007a492454cdd1a950e612ca
sanmesh <sanmeshdesai11@gmail.com>
2026-07-01 19:12:29 +0530

    Restrict cart and checkout to authenticated users

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -45,7 +45,15 @@ const Navbar = () => {
         </ul>
 
         <div className='flex items-center gap-6'>
-            <img onClick={()=>setShowSearch(true)} src = {assets.search_icon} className='w-5 cursor-pointer' alt='' />
+           
+           {token && (
+  <img
+    onClick={() => setShowSearch(true)}
+    src={assets.search_icon}
+    className="w-5 cursor-pointer"
+    alt="Search"
+  />
+)}
             <div className='group relative'>
             
                 <img onClick={()=> token ? null : navigate('/login')} src = {assets.profile_icon} className='w-5 cursor-pointer' alt='' />
@@ -65,10 +73,14 @@ const Navbar = () => {
 
         </div>
 
-        <Link to='/cart' className='relative'>
+        {token && (
+
+            
+            <Link to='/cart' className='relative'>
             <img src={assets.cart_icon} alt="" className='w-5 min-w-5' />
             <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>{getCartCount()}</p>
         </Link>
+)}
 
         <img onClick={()=>setVisible(true)} src={assets.menu_icon} className='w-5 sm:hidden cursor-pointer' alt="" />
         </div>  
